c533ddfdda23c5039887152fadf0495c05ceda4e
sonu-shivcharan <sonushivcharan@outlook.com>
2026-03-15 10:08:26 +0530

    feat: Initialize Next.js project with Tailwind CSS, shadcn/ui, and Clerk authentication.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserAvatar } from "@clerk/nextjs"
+import React from "react"
+
+function DashboardPage() {
+  return (
+    <div>
+      <UserAvatar /> Dashboard Page
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ proxy.ts
@@ -0,0 +1,27 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/profile(.*)"])
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (isProtectedRoute(req)) {
+      const isLoggedIn = (await auth()).isAuthenticated
+      if (!isLoggedIn) {
+        return NextResponse.redirect(new URL("/login", req.url))
+      }
+    }
+  },
+  {
+    afterSignInUrl: "/dashboard",
+    signUpUrl: "/register",
+  }
+)
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+}
